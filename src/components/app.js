b5d2d7ebf544942a7fe081535f1c57ed46faa287
R-Brewster <rbrewster@ucmerced.edu>
2017-11-08 12:52:28 -0800

    Finished fetching data from server

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import TodoList from './todo_list';
 
 const App = () => (
     <div>
         <div className="container">
-           <h1 className="center-align" >To do list 2</h1>
+            <h1 className="center-align" >To do list 2</h1>
+            <TodoList />
         </div>
     </div>
 );
